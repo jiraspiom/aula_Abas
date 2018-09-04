474c4bae4fd75ceacf7f0379175fc8667cede8a6
gilmar <jiraspiom@yahoo.com.br>
2018-09-04 17:03:30 -0300

    PROJETO FINALIZADO

--- app/src/main/java/br/com/example/alalgi/abas/Activities/MainActivity.java
+++ app/src/main/java/br/com/example/alalgi/abas/Activities/MainActivity.java
@@ -1,11 +1,17 @@
 package br.com.example.alalgi.abas.Activities;
 
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
+import br.com.example.alalgi.abas.Fragments.EmAltaFragment;
+import br.com.example.alalgi.abas.Fragments.HomeFragment;
+import br.com.example.alalgi.abas.Fragments.IncricoesFragment;
 import br.com.example.alalgi.abas.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +23,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        smartTabLayout = findViewById(R.id.viewPagertab);
+
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setTitle("Youtube");
+
+        //Configurando as abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Home", HomeFragment.class)
+                .add("Inscrições", IncricoesFragment.class)
+                .add("Em alta", EmAltaFragment.class)
+                .create());
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
     }
+
+
+
+
 }
